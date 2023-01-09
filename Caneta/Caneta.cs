1c00e1cd95de0746a80605f1897ca2ebedab2671
Raisa Almeida <almeida.alves.raisa@gmail.com>
2023-01-08 21:54:48 -0300

    Adicionar arquivos de projeto.

---
+++ Caneta/Caneta.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanetaApp
+{
+    public class Caneta
+    {
+        private string nome;
+
+        private string modelo;
+
+        private string cor;
+
+        private int carga;
+
+        private bool tampada;
+
+        public Caneta(int carga )
+        {
+            this.carga = carga;
+            destampar();
+        }
+
+        public void status()
+        {
+            Console.WriteLine("//////////////////////////////////////////////");
+            Console.WriteLine("Nome: " + this.nome);
+            Console.WriteLine("Modelo: " + this.modelo);
+            Console.WriteLine("Uma caneta: " + this.cor);
+            Console.WriteLine("Carga: " + this.carga);
+            Console.WriteLine("Tampada: " + this.tampada);
+            Console.WriteLine("//////////////////////////////////////////////");
+
+        }
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public void setModelo(string modelo)
+        {
+            this.modelo = modelo;
+        }
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public int somarNum(int num, int num2)
+        {
+          var soma = num + num2;
+            return soma;
+
+        }
+
+
+       
+       public void tampar()
+        {
+            this.tampada = true;
+        }
+
+       public void destampar ()
+        {
+            this.tampada = false;
+        }
+
+        public void setColor(int cor)
+        {
+            if(cor == 1)
+                this.cor= "Rosa";
+
+            else if (cor == 2){
+                this.cor = "Azul";
+            }
+            else
+            {
+                Console.WriteLine("Erro ao adicionar a cor");
+                Console.Error.Close();
+            }
+
+        }
+
+       public void Rabiscar(string palavra)
+        {
+            if (this.carga > 0)
+            {
+                this.carga--;
+                if (this.cor == "Rosa")
+                {
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine(palavra);
+                    Console.ResetColor();
+                }
+                else if (this.cor == "Azul")
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(palavra);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine(palavra);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Caneta sem carga"); 
+            }
+           
+            
+        }
+    }
+}
---
+++ Caneta/Program.cs
@@ -0,0 +1,83 @@
+﻿
+using CanetaApp;
+using System.Collections.Generic;
+using System.Linq;
+
+
+//Caneta c1 = new Caneta();
+//c1.modelo = "bic";
+////c1.cor = "   ";
+//c1.setColor(2);
+//c1.ponta = 0.5f;
+//c1.tampar();
+//var teste =  c1.somarNum(2, 4);
+//c1.status();
+//c1.Rabiscar("Olá Mundo");
+
+var  listaDeCanetas = new List<Caneta>();
+
+
+string opt = "";
+
+do
+{
+    Console.WriteLine("Escolha um das opções");
+    Console.WriteLine("0:Fechar");
+    Console.WriteLine("1: adicionar caneta");
+    Console.WriteLine("2: Mostrar lista");
+    Console.WriteLine("3: Excluir  lista");
+    opt = Console.ReadLine();
+
+    if (opt == "1")
+    {
+        var caneta = new Caneta(2);
+        Console.WriteLine("Digite o nome caneta");
+        caneta.setNome(Console.ReadLine());
+        Console.WriteLine("Digite o modelo da caneta");
+        caneta.setModelo(Console.ReadLine());
+        Console.WriteLine("Digite a cor da caneta- Rosa= 1 e Azul= 2");
+        caneta.setColor(Convert.ToInt32(Console.ReadLine()));
+       //onsole.ReadKey();
+
+
+        listaDeCanetas.Add(caneta);
+
+
+    }
+    if (opt == "2")
+    {
+        foreach (Caneta caneta in listaDeCanetas)
+        {
+            caneta.status();
+        }
+    }
+
+    if (opt == "3")
+    {
+        Console.WriteLine("Digite o nome do item que deseja excluir");
+        var item = Console.ReadLine();
+        Console.ReadKey();
+        var name = Console.ReadLine();
+        Caneta caneta  = listaDeCanetas.Find(x => x.getNome() == name);
+        listaDeCanetas.Remove(caneta);
+        
+    }
+
+    if (opt == "4")
+    {
+        Console.WriteLine("Digite o nome da caneta que você deseja usar ");
+        var name = Console.ReadLine();
+        Caneta caneta = listaDeCanetas.Find(x => x.getNome() == name);
+        Console.WriteLine("Digite a palavra que deseja escrever");
+        var palavra = Console.ReadLine();
+        caneta.Rabiscar(palavra);
+
+
+    }
+
+
+
+
+} while (opt != "0");
+
+Console.WriteLine("Programa finalizado");
